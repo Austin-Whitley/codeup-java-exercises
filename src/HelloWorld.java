313dd696199297ed3715000ad925e89cf3841b6c
Austin Whitley <whitley.austin01@gmail.com>
2021-05-24 11:53:11 -0500

    added lecture notes for Syntax types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,37 @@
 public class HelloWorld {
     public static void main(String[] args){
+        long myNumber = 123L;
+        String myString = " Is my favorite number.";
 
-        System.out.println("Hello, World!");
+        System.out.println(myNumber);
+        System.out.println(myNumber + myString);
+
+//        int intNum = 4500;
+//        long longNum = 450000;
+//
+//        //CASTING:
+//        int intPlusLong = (int)(intNum + longNum);
+//        //you can coerce primitive number types together
+//        //by multiplying them by the desired type (if its smaller)
+//
+//        long longPlusInt = (intNum + longNum);
+//        //since long is the larger variable of the two you dont have to coerce the int
+//        //in the last example we had to coerce the long to become an int
+//
+//        byte iAmAByte = 0;
+//        long iAmALong = 99;
+//        iAmAByte = (byte)iAmALong;
+//        //we can cast something down as long as it will fit inside the scope of that datatype;
+//        System.out.println(iAmAByte);
+//
+//        int iAmAnInt = 27;
+//        double iAmADouble = iAmAnInt;
+//        System.out.println(iAmADouble);
+//
+//        iAmADouble = 3.14159265359;
+//        System.out.println(iAmADouble);
+//        iAmAnInt = (int)iAmADouble;
+//        System.out.println(iAmAnInt);
     }
 }
 
