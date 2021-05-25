631ab215d0bbd893eb2eb82e24c24b3fd37c3a36
Austin Whitley <whitley.austin01@gmail.com>
2021-05-25 13:36:30 -0500

    part 2- b completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,11 +6,11 @@ public class ControlFlowExercises {
             i++;
         }
 
-        int x = 0;
+        int x = 100;
         do{
             System.out.println(x);
-            x = x+2;
-        }while(x <= 100);
+            x = x - 5;
+        }while(x >= -10);
 
 //        long x = 2;
 //        do{
