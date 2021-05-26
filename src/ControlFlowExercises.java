87f8d061664d4a767dee717b3ea8212b095743be
Austin Whitley <whitley.austin01@gmail.com>
2021-05-26 10:00:56 -0500

    added the original power table exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -11,11 +11,13 @@ public class ControlFlowExercises {
             System.out.println(i);
         }
 
+        final int pin = 12345;
+
         for(byte i = 1; i <= 3; i++){
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter Passcode: ");
             int userNum = scanner.nextInt();
-            if(userNum == 12345){
+            if(userNum == pin){
                 System.out.println("Welcome home sir/madam");
                 i = 3;
             }else if(i == 3){
@@ -27,25 +29,41 @@ public class ControlFlowExercises {
         }
 
         int userInput = 0;
-        do{
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Please enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        String userResponse = "";
+        do {
+            System.out.println("What number would you like to go to?");
             userInput = scanner.nextInt();
-            System.out.println("Your number: " + userInput);
-            System.out.println("Here's a table of powers around your number.");
-            System.out.println("Number | Squared | Cubed ");
-            System.out.println(userInput - 2 + "\t\t| " + ((userInput-2) * (userInput-2)) + "\t| " + ((userInput-2) * (userInput-2) * (userInput-2)));
-            System.out.println(userInput - 1 + "\t\t| " + ((userInput-1) * (userInput-1)) + "\t| " + ((userInput-1) * (userInput-1) * (userInput-1)));
-            System.out.println(userInput + "\t\t| " + (userInput * userInput) + "\t| " + (userInput * userInput * userInput));
-            System.out.println(userInput + 1 + "\t\t| " + ((userInput+1) * (userInput+1)) + "\t| " + ((userInput+1) * (userInput+1) * (userInput+1)));
-            System.out.println(userInput + 2 + "\t\t| " + ((userInput+2) * (userInput+2)) + "\t| " + ((userInput+2) * (userInput+2) * (userInput+2)));
 
-            System.out.println("Enter another number: \n (enter -1 to exit)");
-        }while(userInput != -1);
+            System.out.println("Your number: " + userInput);
+            System.out.println("Here's a table of powers to your number.");
+            System.out.println("Number | Squared | Cubed ");
+            for (int i = 0; i <= userInput; i++) {
+                System.out.println(i + "\t\t| " + (i * i) + "\t| " + (i * i * i));
+            }
+            System.out.println("Would you like to continue? Yes/No : ");
+            userResponse = scanner.next();
+        }while(userResponse.equals("yes") || userResponse.equals("Yes") || userResponse.equals("YES"));
+
+//        int userInput = 0;
+//        do{
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Please enter a number: ");
+//            userInput = scanner.nextInt();
+//            System.out.println("Your number: " + userInput);
+//            System.out.println("Here's a table of powers around your number.");
+//            System.out.println("Number | Squared | Cubed ");
+//            System.out.println(userInput - 2 + "\t\t| " + ((userInput-2) * (userInput-2)) + "\t| " + ((userInput-2) * (userInput-2) * (userInput-2)));
+//            System.out.println(userInput - 1 + "\t\t| " + ((userInput-1) * (userInput-1)) + "\t| " + ((userInput-1) * (userInput-1) * (userInput-1)));
+//            System.out.println(userInput + "\t\t| " + (userInput * userInput) + "\t| " + (userInput * userInput * userInput));
+//            System.out.println(userInput + 1 + "\t\t| " + ((userInput+1) * (userInput+1)) + "\t| " + ((userInput+1) * (userInput+1) * (userInput+1)));
+//            System.out.println(userInput + 2 + "\t\t| " + ((userInput+2) * (userInput+2)) + "\t| " + ((userInput+2) * (userInput+2) * (userInput+2)));
+//
+//            System.out.println("Enter another number: \n (enter -1 to exit)");
+//        }while(userInput != -1);
 
         int userGrade = 0;
         do {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("Enter your grade: (0-100)");
             userGrade = scanner.nextInt();
 
