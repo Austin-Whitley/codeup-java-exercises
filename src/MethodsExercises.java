d506afa112a00d39e69cf49d22f3845fa6d99b95
Austin Whitley <whitley.austin01@gmail.com>
2021-05-27 11:12:29 -0500

    updated the multiplication method to work without using *

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,7 +8,11 @@ public class MethodsExercises {
         return num1 - num2;
     }
     public static int Multiplication(int num1, int num2){
-        return num1 * num2;
+        int result = 0;
+        for(int i = 0; i < num2; i++){
+            result = result + num1;
+        }
+        return result;
     }
     public static double Division(double num1, double num2){
         return num1 / num2;
@@ -20,7 +24,7 @@ public class MethodsExercises {
     public static void main(String[] args) {
         System.out.println( "Addition: " + Addition(5, 5));
         System.out.println("Subtraction: " + Subtraction(10,5));
-        System.out.println("Multiplication: " + Multiplication(5,5));
+        System.out.println("Multiplication: " + Multiplication(5,10));
         System.out.println("Division: " + Division(25,5));
         System.out.println("Modulus: " + Modulus(24,5));
     }
