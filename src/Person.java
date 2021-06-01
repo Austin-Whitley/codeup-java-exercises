30bb3de3be447a639afecaa0243305acad54882d
Austin Whitley <whitley.austin01@gmail.com>
2021-06-01 14:28:23 -0500

    added Person class to exercises

---
+++ src/Person.java
@@ -0,0 +1,43 @@
+public class Person {
+
+    private String name;
+
+    //===== setters and getters =====
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //================================
+
+    public static void sayHello(Person p){
+        System.out.println("Hello, " + p.getName());
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+//       Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName())); //true
+//        System.out.println(person1 == person2); //false
+//        sayHello(person1);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+    }
+}
