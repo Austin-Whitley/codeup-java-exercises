b41ea03632d0de9595c735e2e5616c84f464e96d
Austin Whitley <whitley.austin01@gmail.com>
2021-06-05 18:26:00 -0500

    added abstract methods and classes for quadralaterals

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,21 +1,46 @@
 package shapes;
 
-public class Rectangle {
-
-    protected double length;
-    protected double width;
+public class Rectangle extends Quadrilateral implements Measurable{
 
     public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
+        super(length, width);
     }
 
-    public double getArea(){
-        return length * width;
+    @Override
+    public double getPerimeter() {
+        return 0;
     }
 
-    public double getPerimeter(){
-        return ((2 * length) + (2 * width));
+    @Override
+    public double getArea() {
+        return 0;
     }
+
+    @Override
+    public double setLength(double length) {
+        return 0;
+    }
+
+    @Override
+    public double setWidth(double width) {
+        return 0;
+    }
+
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getArea(){
+//        return length * width;
+//    }
+//
+//    public double getPerimeter(){
+//        return ((2 * length) + (2 * width));
+//    }
+
 
 }
--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,13 +2,12 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-
-        Rectangle box1 = new Rectangle(5,4);
-        System.out.println(box1.getArea()); //20
-        System.out.println(box1.getPerimeter()); //18
-
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getArea()); //25
-        System.out.println(box2.getPerimeter()); //20
+//        Rectangle box1 = new Rectangle(5,4);
+//        System.out.println(box1.getArea()); //20
+//        System.out.println(box1.getPerimeter()); //18
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getArea()); //25
+//        System.out.println(box2.getPerimeter()); //20
     }
 }
