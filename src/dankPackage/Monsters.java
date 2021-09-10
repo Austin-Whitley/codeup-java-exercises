1975510b4067fa9e3d36f46cd911884b09a58732
Austin Whitley <whitley.austin01@gmail.com>
2021-09-09 20:20:46 -0500

    added monster class

---
+++ src/dankPackage/Monsters.java
@@ -0,0 +1,39 @@
+package dankPackage;
+
+public class Monsters {
+    private String name;
+    private int health;
+    private String description;
+
+    public Monsters(String name, int health, String description) {
+        this.name = name;
+        this.health = health;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
